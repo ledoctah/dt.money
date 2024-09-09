c53af6412c32c3dc8380750400751e53eeca80cf
Matheus <matheus@matheusferreira.dev>
2024-09-08 21:11:09 -0300

    feat: initialized styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    </ThemeProvider>
   );
 }
