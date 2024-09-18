6b34001a38e3a03859efa809a7074fff6a333360
Matheus <matheus@matheusferreira.dev>
2024-09-17 21:19:15 -0300

    feat: add Transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './style';
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>17/09/2024</td>
+            </tr>
+
+            <tr>
+              <td width="50%">iFood</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>17/09/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
