4b0177c4b3c6fe93795c3c059d3b88e8fdc566a6
Matheus <matheus@matheusferreira.dev>
2024-09-17 21:10:46 -0300

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 
+import { Transactions } from './pages/Transactions';
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
+      <Transactions />
     </ThemeProvider>
   );
 }
