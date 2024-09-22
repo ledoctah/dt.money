6ec6d119ad7d390dae797c5dbf7d0dd3e5da1c4b
Matheus <matheus@matheusferreira.dev>
2024-09-22 10:48:59 -0300

    feat: add summary calc based on transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { SearchForm } from './components/SearchForm';
@@ -9,29 +9,10 @@ import {
   TransactionsTable,
 } from './style';
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  useEffect(() => {
-    async function loadTransactions() {
-      const response = await fetch('http://localhost:3000/transactions');
-
-      const data = await response.json();
-
-      setTransactions(data);
-    }
-
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <div>
